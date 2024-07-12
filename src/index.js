e72287fdbd9fa86cdc0708710efc9d28ddd1c145
laoban <48916435+zhangfuwen1989@users.noreply.github.com>
2024-07-12 16:28:14 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
- "dockerhub.bugaoxing.com": "https://registry-1.docker.io",
+ "docker02.bugaoxing.com": "https://registry-1.docker.io",
  "quay.bugaoxing.com": "https://quay.io",
  "gcr.bugaoxing.com": "https://gcr.io",
  "k8s-gcr.bugaoxing.com": "https://k8s.gcr.io",
