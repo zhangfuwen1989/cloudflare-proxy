c655601bff24a1f8d09ae7d86a799957e887805d
zhangfuwen <415186314@qq.com>
2024-11-18 09:24:11 +0800

    tijiao

--- src/index.js
+++ src/index.js
@@ -3,21 +3,21 @@ addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event.request));
 });
 
+const dockerHub = "https://registry-1.docker.io";
+
 const routes = {
-   "dockerpull.bugaoxing.com": "https://registry-1.docker.io",
- "xiaoyu20241012.bugaoxing.com": "https://registry-1.docker.io",
- "dp.bugaoxing.com": "https://registry-1.docker.io",
- "quay.bugaoxing.com": "https://quay.io",
- "gcr.bugaoxing.com": "https://gcr.io",
- "k8s-gcr.bugaoxing.com": "https://k8s.gcr.io",
- "k8s.bugaoxing.com": "https://registry.k8s.io",
- "ghcr.bugaoxing.com": "https://ghcr.io",
- "cloudsmith.bugaoxing.com": "https://docker.cloudsmith.io",
- "pypi.bugaoxing.com": "https://pypi.org",
- "npmjs.bugaoxing.com": "https://registry.npmjs.org",
- "cnpmjs.bugaoxing.com": "http://r.cnpmjs.org",
- "hub.docker.bugaoxing.com": "https://hub.docker.com",
+  "646f636b6572.boown.com": dockerHub,
+  "71756179.boown.com": "https://quay.io",
+  "676372.boown.com": "https://gcr.io",
+  "6b38732d676372.boown.com": "https://k8s.gcr.io",
+  "6b3873.boown.com": "https://registry.k8s.io",
+  "67686372.boown.com": "https://ghcr.io",
+  "636c6f7564736d697468.boown.com": "https://docker.cloudsmith.io",
+  "70797069.boown.com": "https://pypi.org",
+  "6e706d6a73.boown.com": "https://registry.npmjs.org",
+  "636e706d6a73.boown.com": "http://r.cnpmjs.org",
 };
+
 function routeByHosts(host) {
   if (host in routes) {
     return routes[host];
@@ -29,7 +29,7 @@ function routeByHosts(host) {
 }
 
 async function handleRequest(request) {
-  let url = new URL(request.url);
+  const url = new URL(request.url);
   const upstream = routeByHosts(url.hostname);
   if (upstream === "") {
     return new Response(
@@ -41,48 +41,24 @@ async function handleRequest(request) {
       }
     );
   }
-  if (upstream === "https://registry-1.docker.io") {
-    // Modify URL if necessary based on search parameters and encoded characters
-    if (!/%2F/.test(url.search) && /%3A/.test(url.toString())) {
-      let modifiedUrl = url.toString().replace(/%3A(?=.*?&)/, '%3Alibrary%2F');
-      url = new URL(modifiedUrl);
-    }
-
-    // Append 'library' to the pathname if necessary
-    const libraryPathPattern = /^\/v2\/[^/]+\/[^/]+\/[^/]+$/;
-    const libraryPrefixPattern = /^\/v2\/library/;
-    if (libraryPathPattern.test(url.pathname) && !libraryPrefixPattern.test(url.pathname)) {
-      url.pathname = url.pathname.replace(/\/v2\//, '/v2/library/');
-    }
-  }
-  // check if need to authenticate
+  const isDockerHub = upstream == dockerHub;
+  const authorization = request.headers.get("Authorization");
   if (url.pathname == "/v2/") {
     const newUrl = new URL(upstream + "/v2/");
+    const headers = new Headers();
+    if (authorization) {
+      headers.set("Authorization", authorization);
+    }
+    // check if need to authenticate
     const resp = await fetch(newUrl.toString(), {
       method: "GET",
+      headers: headers,
       redirect: "follow",
     });
-    if (resp.status === 200) {
-    } else if (resp.status === 401) {
-      const headers = new Headers();
-      if (MODE == "debug") {
-        headers.set(
-          "Www-Authenticate",
-          `Bearer realm="${LOCAL_ADDRESS}/v2/auth",service="cloudflare-proxy"`
-        );
-      } else {
-        headers.set(
-          "Www-Authenticate",
-          `Bearer realm="https://${url.hostname}/v2/auth",service="cloudflare-proxy"`
-        );
-      }
-      return new Response(JSON.stringify({ message: "UNAUTHORIZED" }), {
-        status: 401,
-        headers: headers,
-      });
-    } else {
-      return resp;
+    if (resp.status === 401) {
+      return responseUnauthorized(url);
     }
+    return resp;
   }
   // get token
   if (url.pathname == "/v2/auth") {
@@ -99,7 +75,28 @@ async function handleRequest(request) {
       return resp;
     }
     const wwwAuthenticate = parseAuthenticate(authenticateStr);
-    return await fetchToken(wwwAuthenticate, url.searchParams);
+    let scope = url.searchParams.get("scope");
+    // autocomplete repo part into scope for DockerHub library images
+    // Example: repository:busybox:pull => repository:library/busybox:pull
+    if (scope && isDockerHub) {
+      let scopeParts = scope.split(":");
+      if (scopeParts.length == 3 && !scopeParts[1].includes("/")) {
+        scopeParts[1] = "library/" + scopeParts[1];
+        scope = scopeParts.join(":");
+      }
+    }
+    return await fetchToken(wwwAuthenticate, scope, authorization);
+  }
+  // redirect for DockerHub library images
+  // Example: /v2/busybox/manifests/latest => /v2/library/busybox/manifests/latest
+  if (isDockerHub) {
+    const pathParts = url.pathname.split("/");
+    if (pathParts.length == 5) {
+      pathParts.splice(2, 0, "library");
+      const redirectUrl = new URL(url);
+      redirectUrl.pathname = pathParts.join("/");
+      return Response.redirect(redirectUrl, 301);
+    }
   }
   // foward requests
   const newUrl = new URL(upstream + url.pathname);
@@ -108,7 +105,11 @@ async function handleRequest(request) {
     headers: request.headers,
     redirect: "follow",
   });
-  return await fetch(newReq);
+  const resp = await fetch(newReq);
+  if (resp.status == 401) {
+    return responseUnauthorized(url);
+  }
+  return resp;
 }
 
 function parseAuthenticate(authenticateStr) {
@@ -116,7 +117,7 @@ function parseAuthenticate(authenticateStr) {
   // match strings after =" and before "
   const re = /(?<=\=")(?:\\.|[^"\\])*(?=")/g;
   const matches = authenticateStr.match(re);
-  if (matches === null || matches.length < 2) {
+  if (matches == null || matches.length < 2) {
     throw new Error(`invalid Www-Authenticate Header: ${authenticateStr}`);
   }
   return {
@@ -125,13 +126,36 @@ function parseAuthenticate(authenticateStr) {
   };
 }
 
-async function fetchToken(wwwAuthenticate, searchParams) {
+async function fetchToken(wwwAuthenticate, scope, authorization) {
   const url = new URL(wwwAuthenticate.realm);
   if (wwwAuthenticate.service.length) {
     url.searchParams.set("service", wwwAuthenticate.service);
   }
-  if (searchParams.get("scope")) {
-    url.searchParams.set("scope", searchParams.get("scope"));
+  if (scope) {
+    url.searchParams.set("scope", scope);
   }
-  return await fetch(url, { method: "GET", headers: {} });
+  headers = new Headers();
+  if (authorization) {
+    headers.set("Authorization", authorization);
+  }
+  return await fetch(url, { method: "GET", headers: headers });
+}
+
+function responseUnauthorized(url) {
+  const headers = new(Headers);
+  if (MODE == "debug") {
+    headers.set(
+      "Www-Authenticate",
+      `Bearer realm="http://${url.host}/v2/auth",service="cloudflare-proxy"`
+    );
+  } else {
+    headers.set(
+      "Www-Authenticate",
+      `Bearer realm="https://${url.hostname}/v2/auth",service="cloudflare-proxy"`
+    );
+  }
+  return new Response(JSON.stringify({ message: "UNAUTHORIZED" }), {
+    status: 401,
+    headers: headers,
+  });
 }
